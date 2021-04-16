3b23d380ff518e391bf0d2843b709ee1e162ed48
Alejandra Gutierrez <gutihi85@gmail.com>
2021-04-15 20:59:07 -0500

    files

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// get from local storage
+// check local storage for data related to time blcok
+// getitem forw aht youd thing its called
+// if item found then put in input box
+
+
+
+
+// grab the buttons -- addEventListeners
+$('.saveButton').on('click', function() {
+    // set of instuctions
+    console.dir($(this).attr('id'));
+    console.dir(this.id);
+    // When a user clicks a button
+    // -- get the input from the input box
+    // -- store in local storage
+} )
